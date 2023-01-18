136ddf42e18527ec8b53ed587a0141f895593381
Daniel Felipe Suarez Bohórquez <104377939+Danfesu@users.noreply.github.com>
2023-01-18 15:45:17 -0500

    Session 13 Masa Corporal

---
+++ BMI/BMI/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BMI.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public Models.BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new Models.BMI
+            {
+                Height = 180,
+                Width= 73
+            };
+        }
+    }
+}
---
+++ BMI/BMI/MVVM/Views/BMIView.xaml.cs
@@ -0,0 +1,13 @@
+using BMI.MVVM.ViewModels;
+
+namespace BMI.MVVM.Views;
+
+public partial class BMIView : ContentPage
+{
+	public BMIView()
+	{
+		InitializeComponent();
+
+		BindingContext = new BMIViewModel();
+	}
+}
