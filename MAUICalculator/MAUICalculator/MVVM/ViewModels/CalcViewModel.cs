2be9245f05c0f39ad310777582f8454bae8442c7
Daniel Felipe Suarez Bohórquez <104377939+Danfesu@users.noreply.github.com>
2023-01-20 16:35:16 -0500

    Session 17 Calculator

---
+++ MAUICalculator/MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Dangl.Calculator;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MAUICalculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalcViewModel
+    {
+        public string Formula { get; set; } = string.Empty;
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand =>
+            new Command((number) => { Formula += number; });
+
+        public ICommand ResetCommand =>
+            new Command(() =>
+            {
+                Formula = string.Empty;
+                Result = "0";
+            });
+
+        public ICommand BackspaceCommand =>
+            new Command(() =>
+            {
+                if (Formula.Length > 0)
+                {
+                    int count = (char)Formula.ToArray().GetValue(Formula.Length - 1) == ' ' ? 3 : 1;
+                    Formula = Formula.Substring(0, Formula.Length - count);
+                }
+            });
+
+        public ICommand CalculateCommand =>
+            new Command(() =>
+            {
+                if (Formula.Length == 0)
+                    return;
+                var calculator =
+                       Calculator.Calculate(Formula);
+                Result = calculator.Result.ToString();
+
+            });
+    }
+}
---
+++ MAUICalculator/MAUICalculator/MVVM/Views/CalcView.xaml.cs
@@ -0,0 +1,13 @@
+using MAUICalculator.MVVM.ViewModels;
+
+namespace MAUICalculator.MVVM.Views;
+
+public partial class CalcView : ContentPage
+{
+	public CalcView()
+	{
+		InitializeComponent();
+
+		BindingContext = new CalcViewModel();
+	}
+}
